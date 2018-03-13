568b26b4a35dd03e63c6526785c5e8159201c712
giterdone50 <35238783+giterdone50@users.noreply.github.com>
2018-03-13 12:34:42 -0700

    new push

--- src/bst.java
+++ src/bst.java
@@ -1,6 +1,5 @@
-
 public class bst<E extends Comparable<E>>{
-	// root of tre
+	// root of tree
 	Node root;
 
 	// size of tree
@@ -20,53 +19,51 @@ public class bst<E extends Comparable<E>>{
 			left= null;
 			right= null;
 			data= null;
-
 		}
 	}
 
 	// add
 	public void add(E item) {
-		//if the tree is empty, create the root
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>if the tree is empty, create the root
 		if(root==null) {
 			Node newNode = new Node();
 			newNode.data=item;
 			root = newNode;
 			return;
 		}
-		
-		//code for add, maybe useful to have recursive helper method
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>code for add, maybe useful to have recursive helper method
 		addHelper(root, item);
 	}
 
 
 	public boolean addHelper(Node root, E item) {
 		int value = item.compareTo(root.data);
-		// item is same as root's data means already in tree
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>if item is same as root's data means already in tree
 		if(value == 0) {
 			return false;
 		}
-		//if item smaller than root
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   if item smaller than root
 		if(value < 0) {
-			//if no left child
+			//>>>>>>>>>>>>>>>>>>>>>>>>>>>   if no left child then make a left child
 			if(root.left == null) {
 				Node newNode = new Node();
 				root.left = newNode;
 				newNode.data = item;
 			}
-			//if there's left child
+			//  >>>>>>>>>>>>>>>>>>>>>>if there's left child keep going downwards
 			else {
 				addHelper(root.left, item);
 			}
 		}
-		//if item is greater than root
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>if item is greater than root
 		else {
-			//if no right child then make a right child
+			//>>>>>>>>>>>>>>>>>>>>>>>>>if no right child then make a right child
 			if(root.right == null) {
 				Node newNode = new Node();
 				root.right = newNode;
 				newNode.data = item;
 			}
-			//if right child, keep going downwards
+			//>>>>>>>>>>>>>>>>>>>>>>>>>if right child found, keep going downwards
 			else {
 				addHelper(root.right, item);
 			}
@@ -74,9 +71,26 @@ public class bst<E extends Comparable<E>>{
 		return true;
 	}
 	
-	// find element with name item in tree
-	public Node search(E item) {
-		return null;
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> find element with name item in tree
+	public void search(E item) {
+		Node found = searchHelper(root, item);
+		if(found == null) {
+			System.out.println("Not in Tree");
+		}
+		else {
+			System.out.println("Found " + found.data);
+		}
+	}
+	
+	private Node searchHelper(Node root, E item) {
+		//item not in tree or first one is item
+		if(root == null || root.data == item) {
+			return root;
+		}
+		if(root.data.compareTo(item) > 0) {
+			return searchHelper(root.left, item);
+		}
+		return searchHelper(root.right, item);
 	}
 
 	// number of elements in tree
@@ -122,13 +136,20 @@ public class bst<E extends Comparable<E>>{
 		}
 	}
 	
-	public void inorder(Node root) {
+	public void inorder() {
+		if(root == null) {
+			System.out.println("root null");
+		}
+		inorderHelper(root);
+	}
+	
+	private void inorderHelper(Node root) {
 		if(root != null) {
-			System.out.println("HERE" + root.data);
-			inorder(root.left);
-			System.out.println("PUTT" + root.data);
+//			System.out.println("HERE" + root.data);
+			inorderHelper(root.left);
+//			System.out.println("PUTT" + root.data);
 			System.out.println(root.data);
-			inorder(root.right);
+			inorderHelper(root.right);
 		}
 	}
 	
@@ -152,16 +173,17 @@ public class bst<E extends Comparable<E>>{
 	}
 	
 	public static void main(String[] args) {
-		//check each of the methods useing at least 2 different tests
+		//check each of the methods using at least 2 different tests
 		//edge cases, all situations
-		bst <String> newTree = new bst<String>();
-		newTree.add("6");
-		newTree.add("5");
-		newTree.add("3");
-		newTree.add("10");
-		newTree.add("8");
-		newTree.add("12");
-		newTree.add("9");
-		newTree.inorder(newTree.root);	
+		bst <Integer> newTree = new bst<Integer>();
+		newTree.add(Integer.valueOf(6));
+		newTree.add(Integer.valueOf(5));
+		newTree.add(Integer.valueOf(3));
+		newTree.add(Integer.valueOf(10));
+		newTree.add(Integer.valueOf(12));
+		newTree.add(Integer.valueOf(8));
+		newTree.add(Integer.valueOf(9));
+		newTree.inorder();
+		newTree.search(Integer.valueOf(12));
 	}
 }
